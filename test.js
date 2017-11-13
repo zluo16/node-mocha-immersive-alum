d5117e8e1fc1c210de724928b19268e8c3d5112b
Mike <zluo@alumni.berklee.edu>
2017-11-13 13:55:54 -0500

    Done.

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
+let expect = require('chai').expect
+let name = 'React Quickly'
+let url = ['http://reactquickly.co', 'https://www.manning.com/books/react-quickly']
 
-var expect = require('chai').expect
-var name = 'React Quickly'
-var url = ['http://reactquickly.co', 'https://www.manning.com/books/react-quickly']
 describe('name and url', function() {
-  it('must match the values', function(done){
+  it('must be a string', function(done) {
     expect(name).to.be.a('string')
     expect(name).to.equal('React Quickly')
     expect(url).to.have.length(2)
@@ -12,3 +12,20 @@ describe('name and url', function() {
     done()
   })
 })
+
+
+
+
+
+// var name = 'React Quickly'
+// var url = ['http://reactquickly.co', 'https://www.manning.com/books/react-quickly']
+// describe('name and url', function() {
+//   it('must match the values', function(done){
+//     expect(name).to.be.a('string')
+//     expect(name).to.equal('React Quickly')
+//     expect(url).to.have.length(2)
+//     expect(url).to.have.deep.property('[1]', 'https://www.manning.com/books/react-quickly')
+//       .with.length(43)
+//     done()
+//   })
+// })
